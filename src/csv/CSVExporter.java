e72ba90b2c9a2d1d1a1a4cdfb70cefe2343418e6
Divyanshugoyal579 <divyanshugoyal299@gmail.com>
2025-12-16 14:11:41 +0530

    Updated output formatting and plagiarism status

--- src/csv/CSVExporter.java
+++ src/csv/CSVExporter.java
@@ -12,15 +12,22 @@ public class CSVExporter {
 
         try (FileWriter writer = new FileWriter(file, true)) {
 
+          
             if (isNewFile) {
-                writer.append("File1,File2,Method,Similarity(%),Status\n");
+                writer.append("File1,File2,Method,Similarity(%),Status,Lines,Words\n");
             }
 
             writer.append(result.getFile1()).append(",");
             writer.append(result.getFile2()).append(",");
             writer.append(result.getMethod()).append(",");
             writer.append(String.format("%.2f", result.getSimilarity())).append(",");
-            writer.append(result.getStatus()).append("\n");
+            writer.append(result.getStatus()).append(",");
+
+           
+            writer.append(result.getPlagiarizedLines() != null ? result.getPlagiarizedLines().toString() : "")
+                  .append(",");
+            writer.append(result.getPlagiarizedWords() != null ? result.getPlagiarizedWords().toString() : "")
+                  .append("\n");
 
         } catch (IOException e) {
             System.out.println("CSV Write Error: " + e.getMessage());
